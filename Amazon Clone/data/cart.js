3095405f7ce9f50f309156a0c5581baf85309d7a
zhenchez <zhenouc@gmail.com>
2023-10-31 10:33:58 +0100

    added interactive to remove link in checkout

--- Amazon Clone/data/cart.js
+++ Amazon Clone/data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -26,4 +26,10 @@ export function addToCart(productId, quantityNum) {
       quantity: quantityNum,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = cart.filter(cartItem => cartItem.productId !== productId);
+
+  cart = newCart;
 }
--- Amazon Clone/scripts/checkout.js
+++ Amazon Clone/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -38,7 +38,9 @@ cart.forEach(cartItem => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary" data-product-id="${
+              cartItem.productId
+            }">
               Delete
             </span>
           </div>
@@ -89,3 +91,11 @@ cart.forEach(cartItem => {
 });
 
 document.querySelector(".order-summary").innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(".delete-quantity-link").forEach(link => {
+  link.addEventListener("click", () => {
+    const { productId } = link.dataset;
+    removeFromCart(productId);
+    console.log(cart);
+  });
+});
