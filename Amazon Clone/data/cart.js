b37bf057f611241d0062703f735a18eb8b5fe506
zhenchez <zhenouc@gmail.com>
2023-11-01 17:40:43 +0100

    adding delivery options

--- Amazon Clone/data/cart.js
+++ Amazon Clone/data/cart.js
@@ -1,13 +1,4 @@
-export let cart = JSON.parse(localStorage.getItem("cart")) || [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function saveCartToLocalStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
